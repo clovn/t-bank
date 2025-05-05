4127dfd965b069a732347e5ffa938c91337f4caa
Ayrat <57062261+clovn@users.noreply.github.com>
2025-05-05 12:57:52 +0300

    fix validation

--- app/src/main/java/com/example/tbank/presentation/register/RegisterFragment.kt
+++ app/src/main/java/com/example/tbank/presentation/register/RegisterFragment.kt
@@ -5,16 +5,14 @@ import android.view.View
 import androidx.core.widget.addTextChangedListener
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
-import androidx.lifecycle.Lifecycle
-import androidx.lifecycle.lifecycleScope
-import androidx.lifecycle.repeatOnLifecycle
 import androidx.navigation.fragment.findNavController
 import com.example.tbank.R
 import com.example.tbank.databinding.FragmentRegisterBinding
+import com.example.tbank.presentation.observe
 import com.example.tbank.presentation.showError
 import dagger.hilt.android.AndroidEntryPoint
 import dev.androidbroadcast.vbpd.viewBinding
-import kotlinx.coroutines.launch
+
 
 @AndroidEntryPoint
 class RegisterFragment : Fragment(R.layout.fragment_register) {
@@ -27,27 +25,38 @@ class RegisterFragment : Fragment(R.layout.fragment_register) {
 
         initViews()
 
-        lifecycleScope.launch {
-            viewLifecycleOwner.repeatOnLifecycle(Lifecycle.State.STARTED){
-                registerViewModel.uiState.collect { state ->
-                    when(state){
-                        is RegisterState.Idle -> {
-                            binding.registerBtn.isEnabled = true
-                        }
-                        is RegisterState.Loading -> {
-                            binding.registerBtn.isEnabled = false
-                        }
-                        is RegisterState.Success -> {
-                            findNavController().navigate(R.id.action_registerFragment_to_mainFragment)
-                        }
-                    }
+        observeData()
+    }
+
+    private fun observeData(){
+        registerViewModel.uiState.observe(viewLifecycleOwner) { state ->
+            when(state){
+                is RegisterState.Idle -> {
+                    binding.registerBtn.isEnabled = true
+                }
+                is RegisterState.Loading -> {
+                    binding.registerBtn.isEnabled = false
+                }
+                is RegisterState.Success -> {
+                    findNavController().navigate(R.id.action_registerFragment_to_mainFragment)
                 }
             }
+        }
 
-            viewLifecycleOwner.repeatOnLifecycle(Lifecycle.State.STARTED){
-                registerViewModel.errorFlow.collect {error ->
-                    showError(error)
-                }
+        registerViewModel.errorFlow.observe(viewLifecycleOwner) { error ->
+            showError(error)
+        }
+
+        registerViewModel.formState.observe(viewLifecycleOwner) { state ->
+            binding.apply {
+                loginError.visibility = if (state.loginState.isValid) View.GONE else View.VISIBLE
+                firstNameError.visibility = if (state.firstNameState.isValid) View.GONE else View.VISIBLE
+                lastNameError.visibility = if (state.lastNameState.isValid) View.GONE else View.VISIBLE
+                passwordError.visibility = if (state.passwordState.isValid) View.GONE else View.VISIBLE
+                numberError.visibility = if (state.numberState.isValid) View.GONE else View.VISIBLE
+                confirmPasswordError.visibility = if (state.confirmPasswordState.isValid) View.GONE else View.VISIBLE
+
+                registerBtn.isEnabled = state.isRegisterBtnActive
             }
         }
     }
@@ -59,43 +68,41 @@ class RegisterFragment : Fragment(R.layout.fragment_register) {
             }
 
             registerBtn.setOnClickListener {
-                val login = loginEt.text.toString().trim()
-                val firstName = firstNameEt.text.toString().trim()
-                val lastName = lastNameEt.text.toString().trim()
-                val password = passwordEt.text.toString().trim()
-                val number = numberEt.text.toString().trim()
-                val confirmPassword = confirmPasswordEt.text.toString().trim()
+                registerViewModel.register()
+            }
 
-                loginError.visibility = if(isValidLogin(login)) View.GONE else View.VISIBLE
-                firstNameError.visibility = if(isValidName(firstName)) View.GONE else View.VISIBLE
-                lastNameError.visibility = if(isValidName(lastName)) View.GONE else View.VISIBLE
-                passwordError.visibility = if(isValidPassword(password)) View.GONE else View.VISIBLE
-                numberError.visibility = if(isValidNumber(number)) View.GONE else View.VISIBLE
-                confirmPasswordError.visibility = if(password == confirmPassword) View.GONE else View.VISIBLE
+            val currentState = registerViewModel.formState.value
 
-                if(isValidLogin(login) && isValidName(firstName) && isValidName(lastName) && isValidPassword(password) && isValidNumber(number) && password == confirmPassword) registerViewModel.register(loginEt.toString(), firstNameEt.toString(), lastNameEt.toString(), numberEt.toString(), passwordEt.toString(), confirmPasswordEt.toString())
+            loginEt.apply {
+                setText(currentState.loginState.value)
+                addTextChangedListener { registerViewModel.onLoginChanged(it.toString().trim()) }
+            }
+
+            firstNameEt.apply {
+                setText(currentState.firstNameState.value)
+                addTextChangedListener { registerViewModel.onFirstNameChanged(it.toString().trim()) }
+            }
+
+            lastNameEt.apply {
+                setText(currentState.lastNameState.value)
+                addTextChangedListener { registerViewModel.onLastNameChanged(it.toString().trim()) }
+            }
+
+            numberEt.apply {
+                setText(currentState.numberState.value)
+                addTextChangedListener { registerViewModel.onNumberChanged(it.toString().trim()) }
+            }
+
+            passwordEt.apply {
+                setText(currentState.passwordState.value)
+                addTextChangedListener { registerViewModel.onPasswordChanged(it.toString().trim()) }
+            }
+
+            confirmPasswordEt.apply {
+                setText(currentState.confirmPasswordState.value)
+                addTextChangedListener { registerViewModel.onConfirmPasswordChanged(it.toString().trim()) }
             }
         }
     }
 
-
-    private fun isValidLogin(login: String): Boolean {
-        val regex = "^[a-zA-Z0-9_-]{3,20}$".toRegex()
-        return regex.matches(login)
-    }
-
-    private fun isValidName(name: String): Boolean {
-        val regex = "^[а-яА-Я]{3,20}$".toRegex()
-        return regex.matches(name)
-    }
-
-    private fun isValidPassword(password: String): Boolean {
-        val regex = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)[a-zA-Z\\d!@#$%&*()]{8,}$".toRegex()
-        return regex.matches(password)
-    }
-
-    private fun isValidNumber(phoneNumber: String): Boolean {
-        val regex = "^[78]\\d{10}$".toRegex()
-        return regex.matches(phoneNumber)
-    }
 }
